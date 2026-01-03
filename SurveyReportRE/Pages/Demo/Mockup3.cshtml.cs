79fd694a41c9c0f88c77ff5178e9c2b9fb247493
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-01-03 18:36:40 +0700

    main: Update Mockup3

---
+++ SurveyReportRE/Pages/Demo/Mockup3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JogetMVC.Pages.Demo
+{
+    public class Mockup3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
