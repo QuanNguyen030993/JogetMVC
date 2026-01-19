8bc528b075e28e267c87fa81f66d970c19e11fb7
quannh6 <quan.nh@tokiomarine.com.vn>
2026-01-19 13:48:00 +0700

    feature:  Source split view for the main quotation view

---
+++ SurveyReportRE/Pages/Business/Form/QuotationDetail/_Quotation_LMKT_Form.cshtml.cs
@@ -0,0 +1,33 @@
+//using ERPCore.Models.Base;
+//using ERPCore.Models.Models.Parsing;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.SharePoint.WebControls;
+
+//namespace ERPCore.Pages
+//{
+//    public class Quotation_LMKT_FormModel : PageModel
+//    {
+//        //private readonly ILogger<Quotation_FormModel> _logger;
+//        public static string ModelName { get; set; } = "";
+//        private static string Id { get; set; }
+
+//        private readonly IBaseRepository<Quotation> _quotationRepository;
+
+//        public Quotation_LMKT_FormModel(ILogger<Quotation_LMKT_FormModel> logger, IConfiguration configuration, Microsoft.Extensions.Options.IOptionsMonitor<BlobStorageSettings> blobStorageSettings, IHttpContextAccessor httpContextAccessor)
+//        {
+//            _quotationRepository = new BaseRepository<Quotation>(configuration, httpContextAccessor);
+//            //_logger = logger;
+//        }
+//        public async void OnGet(int? pageNum)
+//        {
+//            if (pageNum != 0)
+//            {
+
+//            }
+
+//            ModelName = nameof(Quotation);
+//            ViewData[nameof(Id)] = pageNum ?? 0;
+//        }
+//    }
+//}
