f55321e908df2a9de67c2a92478265edab577b74
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-03-21 22:28:20 +0700

    feature: Overview Pane split

---
+++ SurveyReportRE/Model/OverviewPanePartialModel.cs
@@ -0,0 +1,6 @@
+﻿public class OverviewPanePartialModel
+{
+    public string Prefix { get; set; } = "qt";
+    public string ModuleCode { get; set; } = "qt"; // qt | pi
+    public string Title { get; set; } = "Overview";
+}
