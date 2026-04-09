b5ec95a1761111e8bb3362e30371411875d2c2f2
quannh6 <quan.nh@tokiomarine.com.vn>
2026-04-09 11:25:41 +0700

    feature: Split request type form, reorder somefield,

---
+++ SurveyReportRE/Pages/Business/Form/Popup/PolicyIssuance_Request_Form.cshtml.cs
@@ -0,0 +1,32 @@
+using ERPCore.Models.Base;
+using ERPCore.Models.Models.Parsing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.SharePoint.WebControls;
+
+namespace ERPCore.Pages
+{
+    public class PolicyIssuance_Request_FormModel : PageModel
+    {
+        //private readonly ILogger<PolicyIssuance_FormModel> _logger;
+        public static string ModelName { get; set; } = "";
+        private static string Id { get; set; }
+
+        private readonly IBaseRepository<PolicyIssuance> _PolicyIssuanceRepository;
+
+        public PolicyIssuance_Request_FormModel(ILogger<PolicyIssuance_Request_FormModel> logger, IConfiguration configuration, Microsoft.Extensions.Options.IOptionsMonitor<BlobStorageSettings> blobStorageSettings, IHttpContextAccessor httpContextAccessor)
+        {
+            _PolicyIssuanceRepository = new BaseRepository<PolicyIssuance>(configuration, httpContextAccessor);
+            //_logger = logger;
+        }
+        public async void OnGet(int? pageNum)
+        {
+            if (pageNum != 0)
+            {
+
+            }
+            ModelName = nameof(PolicyIssuance);
+            ViewData[nameof(Id)] = pageNum ?? 0;
+        }
+    }
+}
