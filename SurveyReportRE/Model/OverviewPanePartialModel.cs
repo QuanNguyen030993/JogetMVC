6b792fe886f2617411aa72ae35e187627903f8b4
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-05-03 19:56:41 +0700

    feature-demo: Id add

--- SurveyReportRE/Model/OverviewPanePartialModel.cs
+++ SurveyReportRE/Model/OverviewPanePartialModel.cs
@@ -2,5 +2,6 @@
 {
     public string Prefix { get; set; } = "qt";
     public string ModuleCode { get; set; } = "qt"; // qt | pi
+    public string Id { get; set; } = "";
     public string Title { get; set; } = "Overview";
 }
