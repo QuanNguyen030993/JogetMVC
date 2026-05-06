11531307641deb09a906acdaa5553ec7fe979446
quannh6 <quan.nh@tokiomarine.com.vn>
2026-05-06 13:07:08 +0700

    feature: detail in Quotation

---
+++ RESurveyTool.Models/Models/Migration/Business/Data/QuotationSubDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using ERPCore.Models.Migration.Base;
+using ERPCore.Models.Migration.Business.Data;
+using ERPCore.Models.Migration.Business.MasterData;
+using ERPCore.Models.Migration.Config;
+
+public class QuotationSubDetails : BaseModel
+{
+    
+}
+
