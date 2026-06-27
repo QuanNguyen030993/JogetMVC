7a7b9915b57c7fffcb1839194556d7246c737d30
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-06-27 10:40:12 +0700

    June-2026: GetAssignableByGroupRequest add

---
+++ RESurveyTool.Models/Models/Request/GetAssignableByGroupRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogetMVC.Models.Models.Request
+{
+    public class GetAssignableByGroupRequest
+    {
+        public string? group { get; set; }
+        public string? branchCode { get; set; }
+        public bool? excludeCurrent { get; set; } = true;
+        public string? keyword { get; set; } = "";
+    }
+}
