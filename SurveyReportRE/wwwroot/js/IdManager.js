4f662d60e5a6468617e8526caa07e85a8c245fdc
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-08-07 06:59:50 +0700

    Augu-2026: Idmanager attachment handle

--- SurveyReportRE/wwwroot/js/IdManager.js
+++ SurveyReportRE/wwwroot/js/IdManager.js
@@ -3,9 +3,43 @@
 // across different sections (header, upperbody, form, lowerbody, footer) and departments (FO, TS, UW, LMKT, PM)
 
 window.IdManager = {
-    cd: function (name, prefix)  { return `${prefix}-${name}`; }, 
-    attachmentPreviewList: function (name, prefix, dept, type, quotationId) { return `attList_${this.cd(name, prefix)}_${dept}__${type}_${quotationId}`;; },
-    attachmentControl: function (name, prefix, dept, type, quotationId) { return `fileUpload_${this.cd(name, prefix)}_${dept}__${type}_${quotationId}`; }, 
+    cd: function (name, prefix) { return `${prefix}-${name}`; },
+    attachmentKey: function (name, prefix, dept, type, quotationId) {
+        return `${this.cd(name, prefix)}_${dept}__${type}_${quotationId}`;
+    },
+    attachmentPreviewBase: function (controlId) { return `attList_${controlId}`; },
+    attachmentControlBase: function (controlId) { return `fileUpload_${controlId}`; },
+    attachmentSectionKey: function (sectionId, type, recordId) {
+        return `${sectionId}_${type}_${recordId}`;
+    },
+    attachmentPreviewList: function (name, prefix, dept, type, quotationId) {
+        return this.attachmentPreviewBase(this.attachmentKey(name, prefix, dept, type, quotationId));
+    },
+    attachmentControl: function (name, prefix, dept, type, quotationId) {
+        return this.attachmentControlBase(this.attachmentKey(name, prefix, dept, type, quotationId));
+    },
+    attachmentPreviewHost: function (controlId, renderTime) {
+        return `${this.attachmentPreviewBase(controlId)}_${renderTime}`;
+    },
+    attachmentControlHost: function (controlId, renderTime) {
+        return this.fileUploaderId(controlId, renderTime);
+    },
+    fileUploaderId: function (context, instanceId) { return `fileUpload_${context}_${instanceId}`; },
+    attachmentHostIds: function (controlId, renderTime) {
+        return {
+            preview: this.attachmentPreviewHost(controlId, renderTime),
+            uploader: this.attachmentControlHost(controlId, renderTime)
+        };
+    },
+    attachmentPreviewItemsSelector: function (name, prefix, dept, type, quotationId) {
+        return `[id^='${this.attachmentPreviewList(name, prefix, dept, type, quotationId)}_'] .att-item`;
+    },
+    attachmentPreviewItemsByControlIdSelector: function (controlId) {
+        return `[id^='${this.attachmentPreviewBase(controlId)}_'] .att-item`;
+    },
+    attachmentControlHostsSelector: function () {
+        return `[id^='fileUpload_']`;
+    }
 };
 
 // Example usage:
