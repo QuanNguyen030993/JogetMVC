bb69f77ff38fb3caec1cf71361a6b98ef64590d0
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-08-24 06:27:20 +0700

    Augu-2026: Refactor array quotation and policy issuance array

---
+++ SurveyReportRE/Common/DepartmentModuleCodes.cs
@@ -0,0 +1,8 @@
+
+
+public static class DepartmentModuleCodes
+{
+    public static readonly string[] Quotation = ["FO", "TS", "UW", "LMKT", "PM"];
+
+    public static readonly string[] PolicyIssuance = ["FO", "TS", "PM"];
+}
